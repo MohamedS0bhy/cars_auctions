6665b710e36147393519cd49f6ba7c501ff0ce0d
MohamedS0bhy <mohamedsobhy2296@gmail.com>
2018-11-01 13:52:01 +0200

    add oauth keys

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -47,6 +47,7 @@ class SocialAuthController extends Controller
 	    		$existUser = new User();
 	    		$existUser->username = $user->getName();
 	    		$existUser->email = $user->getEmail();
+	    		$existUser->password = Hash::make($user->getId());
 	    		$existUser->save();
 	    	}
 
